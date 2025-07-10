b2b46febfd107bd1be15d8c62cd8591081fa3c6c
Raul Vila <raul.vila.roca@gmail.com>
2025-07-10 23:00:55 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'oc1234567890',
+  'passwordsalt' => 'some-random-salt',
+  'secret' => 'another-random-secret',
+  'trusted_domains' => 
+  array (
+    0 => 'localhost',
+    1 => 'tfgraul',
+  ),
+  'trusted_proxies' => 
+  array (
+    0 => '172.27.10.2',
+  ),
+  'datadirectory' => '/var/www/html/data',
+  'overwrite.cli.url' => 'https://localhost/nextcloud',
+  'overwritehost' => 'localhost',
+  'overwriteprotocol' => 'https',
+  'overwritewebroot' => '/nextcloud',
+  'htaccess.RewriteBase' => '/nextcloud',
+  'dbtype' => 'mysql',
+  'dbname' => 'nextcloud',
+  'dbhost' => 'db',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'nextcloud',
+  'dbpassword' => 'example',
+  'installed' => false,
+);
